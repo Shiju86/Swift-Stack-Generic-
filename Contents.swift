40267bceb87d59e1f668b0db40c96f1a6946548a
Shiju Varghese <shiju.varghese@HYDDT0004-iMac.local>
2019-10-18 12:47:36 +0530

    Using Generic functions for incomatible types

--- Contents.swift
+++ Contents.swift
@@ -25,3 +25,28 @@ print(obj)
 
 obj.pop()
 print(obj)
+
+
+// Using Generic functions for incomatible type
+/*
+ func -(lhs: Self, rhs: Self) -> Self
+ func *(lhs: Self, rhs: Self) -> Self
+ func /(lhs: Self, rhs: Self) -> Self
+ func %(lhs: Self, rhs: Self) -> Self
+ */
+ 
+protocol Add {
+    static func +(lhs: Self, rhs: Self) -> Self
+}
+
+func adding<T: Add>(a: T, b: T) -> T {
+    return a + b
+}
+
+extension Int: Add {}
+extension String: Add {}
+extension Double: Add {}
+
+print(adding(a: 5, b: 5))
+print(adding(a: "Sam", b: "Winchester"))
+print(adding(a: 5.2, b: 5.2))
